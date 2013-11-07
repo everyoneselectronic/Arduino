f2cba362e9984dfe2d71424b1a0ad6517dbf7dfb
everyoneselectronic <fogertyliam@gmail.com>
2013-11-07 17:03:39 +0000

    Update sample2.ino
    added example of "minutes"

--- libraries/StopWatch/examples/sample2/sample2.ino
+++ libraries/StopWatch/examples/sample2/sample2.ino
@@ -3,6 +3,7 @@
 StopWatch sw_millis;    // MILLIS (default)
 StopWatch sw_micros(StopWatch::MICROS);
 StopWatch sw_secs(StopWatch::SECONDS);
+StopWatch sw_mins(StopWatch::MINUTES);
 
 void setup() 
 {
@@ -10,17 +11,20 @@ void setup()
     sw_millis.start();
     sw_micros.start();
     sw_secs.start();
+    sw_mins.start();
 }
 
 
 void loop() 
 {
-    Serial.print("sw_millis=");
-    Serial.println(sw_millis.elapsed());
     Serial.print("sw_micros=");
     Serial.println(sw_micros.elapsed());
+    Serial.print("sw_millis=");
+    Serial.println(sw_millis.elapsed());
     Serial.print("sw_secs=");
     Serial.println(sw_secs.elapsed());
-   
+    Serial.print("sw_mins=");
+    Serial.println(sw_mins.elapsed());
+    Serial.println(" ");
     delay(1000);
 }
